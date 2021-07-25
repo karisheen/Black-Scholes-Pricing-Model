a221cf0cc09b47e6c3e1b45e1b110bc4071ed6d1
karisheen <dakariantoine@gmail.com>
2021-07-25 13:40:07 -0500

    first commit

---
+++ BlackScholesPricingModel.py
@@ -0,0 +1,13 @@
+#Black Scholes Pricing Model
+
+from math import sqrt, exp, log, erfc
+
+from decimal import *
+getcontext () .prec = 5
+
+
+#Underlying Stock Price
+#Option Strike Price
+#Expiry Date
+#Risk Free Interest Rate
+#Volatility of Stock
