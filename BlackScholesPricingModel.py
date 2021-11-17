0ce13d3d1830deee4f287604b093d1eeedde2e45
karisheen <dakariantoine@gmail.com>
2021-11-17 15:13:36 -0600

    created classes for call/put options

--- BlackScholesPricingModel.py
+++ BlackScholesPricingModel.py
@@ -9,4 +9,27 @@ class EuropeanCall:
         volatility):
 
         b = math.exp(risk_free_rate * time_to_maturity)
-        x1 = math.log(asset_price / strike_price)
+        x1 = math.log(asset_price / (b*strike_price)) + .5 *(volatility * volatility * volatility) * time_to_maturity
+        x1 = x1 / (volatility * (time_to_maturity ** .5))
+        z1 = norm.cdf(x1)
+        z1 = z1 * asset_price
+        x2 = math.log(asset_price / (b*strike_price)) - .5 *(volatility * volatility * volatility) * time_to_maturity
+        x2 = x2 / (volatility * (time_to_maturity ** .5))
+        z2 = norm.cdf(x2)
+        z2 = b * strike_price * z2
+        return z1 - z2
+
+        def __init__(self, asset_price, strike_price, time_to_maturity, risk_free_rate, volatility):
+            self.asset_price = asset_price
+            self.strike_price = strike_price
+            self.time_to_maturity = time_to_maturity
+            self.risk_free_rate = risk_free_rate
+            self.volatility = volatility
+            self.price = self.call_price(asset_price, strike_price, time_to_maturity, risk_free_rate, volatility)
+
+class EuropeanPut:
+    def put_price(
+        self, asset_price, strike_price, time_to_maturity, risk_free_rate,
+        volatility):
+
+        
