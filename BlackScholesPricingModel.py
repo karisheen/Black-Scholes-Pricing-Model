e6ca9730ab3d0d9566a8191e62e59d5838e71b2f
Dakari Antoine <dra72@txstate.edu>
2023-07-10 14:26:51 -0500

    added functionality to use real stock data

--- BlackScholesPricingModel.py
+++ BlackScholesPricingModel.py
@@ -30,3 +30,31 @@ def bs_call(S, K, T, r, sigma):
 # Define the Black Scholes Put Option formula
 def bs_put(S, K, T, r, sigma):
     return K * exp(-r * T) - S + bs_call(S, K, T, r, sigma)
+
+# Ask the user if the want to use real stock data or manually input the variables
+data_choice = input ("Do you want to use real stock data or manually input the variables? (r/m): ")
+
+if data_choice == "r":
+    # Fetch real stock data and calculate option prices
+    stock = str(input("Enter the stock ticker: "))
+    current_price = round(web.DataReader(stock, "yahoo")["Adj Close"].iloc[-1], 2)
+    print("The current price of", stock, " is: ", current_price)
+    choice = input("Wanna price a call or a put ? (c/p): ")
+    expiry = str(input("select the expiry date (format mm-dd-YYYY): "))
+    strike_price = int(input("select the strike price: "))
+    today = datetime.now()
+    one_year_ago = today.replace(year=today.year - 1)
+    df = web.DataReader(stock, "yahoo", one_year_ago, today)
+    df = df.sort_values(by="Date")
+    df = df.dropna()
+    df = df.assign(close_day_before=df.Close.shift(1))
+    df["returns"] = (df.Close - df.close_day_before) / df.close_day_before
+    sigma = np.sqrt(252) * df["returns"].std()
+    ty10y = (web.DataReader("^TNX", "yahoo")["Close"].iloc[-1]) / 100
+    last_close = df["Close"].iloc[-1]
+    t = (datetime.strptime(expiry, "%m-%d-%Y") - datetime.utcnow()).days / 365
+
+    if choice == "c":
+        print("The Call Price is: ", bs_call(last_close, strike_price, t, ty10y, sigma))
+    if choice == "p":
+        print("The Put Price is: ", bs_put(last_close, strike_price, t, ty10y, sigma))
