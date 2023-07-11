a8621a74d8ca7adf422db0b3eb44313a6267be68
Dakari Antoine <dra72@txstate.edu>
2023-07-11 03:04:27 -0500

    deleted test comment

--- BlackScholesPricingModel.py
+++ BlackScholesPricingModel.py
@@ -12,7 +12,7 @@ import yfinance as yf
 # T = time to maturity
 # r = risk-free rate
 # sigma = volatility
-# this is a test comment to determine if I am on the right github account
+
 
 # Define the d1 and d2 functions
 
